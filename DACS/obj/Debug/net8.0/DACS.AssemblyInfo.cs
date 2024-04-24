9597d018a67bc4fdcdc704ad2267080a4d1ba9e8
LucxHuynh <huynhtanloc11318@gmail.com>
2024-04-24 11:49:06 +0700

    uploadchil

--- DACS/obj/Debug/net8.0/DACS.AssemblyInfo.cs
+++ DACS/obj/Debug/net8.0/DACS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DACS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+746801ee4d8ee2a7b217502f3812cd3fdd7acf75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b0297758d4fb529ff7837e183516721e934c77")]
 [assembly: System.Reflection.AssemblyProductAttribute("DACS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DACS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
